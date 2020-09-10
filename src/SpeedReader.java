8edfe2a1a180720d9d82e7a3f6ba364eb7b37a37
Alain Kägi <16820273+alainkaegi@users.noreply.github.com>
2020-09-10 15:07:58 -0700

    Add the SpeedReader and an input file.

---
+++ src/SpeedReader.java
@@ -0,0 +1,24 @@
+public class SpeedReader {
+
+    public static void main(String[] args) {
+        while (true) {
+            In file = new In("speed.txt");
+            while (!file.isEmpty()) {
+                String s = file.readLine();
+                int i = 0;
+                while  (i < s.length()) {
+                    String w = "";
+                    while (i < s.length() && s.charAt(i) != ' ') {
+                        w = w + s.charAt(i);
+                        ++i;
+                    }
+                    ++i;
+                    StdDraw.clear();
+                    StdDraw.text(0.5, 0.5, w);
+                    StdDraw.pause(300);
+                }
+            }
+            StdDraw.pause(500);
+        }
+    }
+}
