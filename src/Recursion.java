7913d1b5450c54e32b418e648f92de9e7dff0916
Alain Kägi <16820273+alainkaegi@users.noreply.github.com>
2020-10-08 16:48:00 -0700

    Compare the performance of factorial() and fibonacci().

--- src/Recursion.java
+++ src/Recursion.java
@@ -1,12 +1,40 @@
 public class Recursion {
 
-    public static void main(String[] unused) {
-        StdOut.println("Factorial of 1 is " + factorial(1));
-        StdOut.println("Factorial of 5 is " + factorial(5));
-        StdOut.println("Factorial of 10 is " + factorial(10));
-        StdOut.println("Fibonacci of 1 is " + fibonacci(1));
-        StdOut.println("Fibonacci of 5 is " + fibonacci(5));
-        StdOut.println("Fibonacci of 10 is " + fibonacci(10));
+    public static void main(String[] args) {
+        runFactorials();
+        runFibonaccis();
+    }
+
+    public static void runFactorials() {
+        Stopwatch timer = new Stopwatch();
+        factorial(1);
+        double time1 = timer.elapsedTime();
+        factorial(5);
+        double time5 = timer.elapsedTime();
+        factorial(10);
+        double time10 = timer.elapsedTime();
+        factorial(50);
+        double time50 = timer.elapsedTime();
+        StdOut.printf("factorial(1) took %.2f seconds\n", time1);
+        StdOut.printf("factorial(5) took %.2f seconds\n", time5 - time1);
+        StdOut.printf("factorial(10) took %.2f seconds\n", time10 - time5);
+        StdOut.printf("factorial(50) took %.2f seconds\n", time50 - time10);
+    }
+
+    public static void runFibonaccis() {
+        Stopwatch timer = new Stopwatch();
+        fibonacci(1);
+        double time1 = timer.elapsedTime();
+        fibonacci(5);
+        double time5 = timer.elapsedTime();
+        fibonacci(10);
+        double time10 = timer.elapsedTime();
+        fibonacci(50);
+        double time50 = timer.elapsedTime();
+        StdOut.printf("fibonacci(1) took %.2f seconds\n", time1);
+        StdOut.printf("fibonacci(5) took %.2f seconds\n", time5 - time1);
+        StdOut.printf("fibonacci(10) took %.2f seconds\n", time10 - time5);
+        StdOut.printf("fibonacci(50) took %.2f seconds\n", time50 - time10);
     }
 
     public static long factorial(long n) {
