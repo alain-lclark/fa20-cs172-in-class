1297d94bb7d6f511c5eb86c68dc7845c04f8e187
Alain Kägi <16820273+alainkaegi@users.noreply.github.com>
2020-10-22 17:37:35 -0700

    Add a client for our complex number library.

---
+++ src/ComplexClient.java
@@ -0,0 +1,9 @@
+public class ComplexClient {
+
+    public static void main(String[] unused) {
+        Complex c1 = new Complex(1.0, 0.0);
+        Complex c2 = new Complex(-3.2, +6.17);
+        StdOut.println(c1.add(c2));
+    }
+
+}
