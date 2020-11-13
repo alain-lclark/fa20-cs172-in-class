0db4de2f48dee1f31e9b60fbd260a70e131bcb63
Alain Kägi <16820273+alainkaegi@users.noreply.github.com>
2020-11-12 17:12:08 -0800

    Make our Insertion sort work for any type that implements Comparable.

--- test/InsertionTest.java
+++ test/InsertionTest.java
@@ -6,7 +6,7 @@ class InsertionTest {
 
     @Test
     void sort() {
-        int a[] = { 5, 3, 1, 4 };
+        Integer[] a = { 5, 3, 1, 4 };
         Insertion.sort(a);
         assertTrue(Insertion.sorted(a, 0, a.length - 1));
     }
