d9c8c67a715551f28e5e566e357d429f347b9ee9
Alain Kägi <16820273+alainkaegi@users.noreply.github.com>
2020-11-12 17:21:17 -0800

    Test our Insertion sort on an array of Strings.

--- test/InsertionTest.java
+++ test/InsertionTest.java
@@ -9,6 +9,10 @@ class InsertionTest {
         Integer[] a = { 5, 3, 1, 4 };
         Insertion.sort(a);
         assertTrue(Insertion.sorted(a, 0, a.length - 1));
+
+        String[] s = { "zebra", "hello", "biscuit", "alligator" };
+        Insertion.sort(s);
+        assertTrue(Insertion.sorted(s, 0, s.length - 1));
     }
 
 }
