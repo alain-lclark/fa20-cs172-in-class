3996d38b1671312d4ef83aa88fccfc25651250f6
Alain Kägi <16820273+alainkaegi@users.noreply.github.com>
2020-12-01 15:31:30 -0800

    Implemented a Linked List.

--- src/List.java
+++ src/List.java
@@ -1,8 +1,34 @@
 public interface List {
 
+    /**
+     * Returns the element at position index.
+     *
+     * It is assumed that index is at least 0 and less than the list's size.
+     *
+     * @param index  position in the list
+     * @return the element at the given index
+     */
     public abstract String get(int index);
+
+    /**
+     * Adds item to the end of the list.
+     * @param item  item to be added
+     */
     public abstract void add(String item);
+
+    /**
+     * Removes the element at position index.
+     *
+     * It is assumed that index is at least 0 and less than the list's size.
+     *
+     * @param index  position in the list
+     */
     public abstract void removeAt(int index);
+
+    /**
+     * Returns the number of elements in this list.
+     * @return the number of elements in this list.
+     */
     public abstract int size();
 
 }
---
+++ test/LinkedListTest.java
@@ -0,0 +1,74 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LinkedListTest {
+
+    @Test
+    void testToString() {
+        List l  = new LinkedList();
+        l.add("a");
+        l.add("b");
+        l.add("c");
+        assertEquals("[\"a\", \"b\", \"c\", ]", l.toString());
+    }
+
+    @Test
+    void testEquals() {
+        List l1  = new LinkedList();
+        l1.add("a");
+        l1.add("b");
+        l1.add("c");
+        List l2 = new LinkedList();
+        l2.add("a");
+        l2.add("b");
+        l2.add("c");
+        assertEquals(l1, l2);
+
+        List l3 = new LinkedList();
+        l3.add("a");
+        l3.add("b");
+        l3.add("z");
+        assertNotEquals(l1, l3);
+
+        List l4 = new LinkedList();
+        l4.add("a");
+        l4.add("b");
+        assertNotEquals(l1, l4);
+    }
+
+    @Test
+    void testAddAndGet() {
+        List l  = new LinkedList();
+        l.add("a");
+        l.add("b");
+        l.add("c");
+        assertEquals("a", l.get(0));
+        assertEquals("b", l.get(1));
+        assertEquals("c", l.get(2));
+    }
+
+    @Test
+    void testSize() {
+        List l  = new LinkedList();
+        assertEquals(0, l.size());
+        l.add("a");
+        l.add("b");
+        l.add("c");
+        assertEquals(3, l.size());
+    }
+
+    @Test
+    void testRemoveAt() {
+        List l  = new LinkedList();
+        l.add("a");
+        l.add("b");
+        l.add("c");
+        l.removeAt(1);
+        assertEquals("a", l.get(0));
+        assertEquals("c", l.get(1));
+        l.removeAt(0);
+        assertEquals("c", l.get(0));
+    }
+
+}
